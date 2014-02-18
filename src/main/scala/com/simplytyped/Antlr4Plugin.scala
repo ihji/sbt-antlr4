9cc76278e1854533090376c8184a16ad96bde1d4
Heejong Lee <heejong@gmail.com>
2014-02-18 17:37:08 +0900

    upgrade antlr dependency to 4.2

--- src/main/scala/com/simplytyped/Antlr4Plugin.scala
+++ src/main/scala/com/simplytyped/Antlr4Plugin.scala
@@ -52,7 +52,7 @@ object Antlr4Plugin extends Plugin {
     sourceDirectory <<= (sourceDirectory in Compile) {_ / "antlr4"},
     javaSource <<= sourceManaged in Compile,
     antlr4Generate <<= antlr4GeneratorTask,
-    antlr4Dependency := "org.antlr" % "antlr4" % "4.1",
+    antlr4Dependency := "org.antlr" % "antlr4" % "4.2",
     antlr4PackageName := None,
     antlr4GenListener := true,
     antlr4GenVisitor := false
